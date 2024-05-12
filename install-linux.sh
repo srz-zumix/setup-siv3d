88b53bf42ac6fddffb668f194ee00b620535fc62
srz_zumix <zumix.cpp@gmail.com>
2024-05-13 00:46:53 +0900

    setup linux (#5)

---
+++ install-linux.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -euox pipefail
+
+source "${GITHUB_ACTION_PATH:-.}/resolve.sh"
+
+mkdir -p "${SIV3D_INSTALLDIR}"
+SIV3D_IMAGE="ghcr.io/srz-zumix/setup-siv3d:${VERSION}"
+SIV3D=${SIV3D_INSTALLDIR}/siv3d_${VERSION}_linux
+INSTALL_PATH="${INSTALL_PATH:-./usr/local}"
+
+install_deps() {
+  echo '::group::📖 Install dependencies ...'
+  sudo apt-get update
+  sudo apt-get install -y --no-install-recommends \
+    libasound2-dev \
+    libavcodec-dev \
+    libavformat-dev \
+    libavutil-dev \
+    libcurl4-openssl-dev \
+    libgtk-3-dev \
+    libgif-dev \
+    libglu1-mesa-dev \
+    libmpg123-dev \
+    libopencv-dev \
+    libopus-dev \
+    libopusfile-dev \
+    libsoundtouch-dev \
+    libturbojpeg0-dev \
+    libvorbis-dev \
+    libwebp-dev
+  echo '::endgroup::'
+}
+
+install() {
+  echo '::group::📖 Download Siv3D image ...'
+  docker pull "${SIV3D_IMAGE}"
+  echo '::endgroup::'
+  echo '::group::📖 Copy Siv3D ...'
+  docker run --rm -v "${SIV3D}:/siv3d" --entrypoint bash "${SIV3D_IMAGE}" -c "cp -r /usr/local/lib /siv3d/lib && cp -r /usr/local/include /siv3d/include"
+  ls -l "${SIV3D}/lib"
+  ls -l "${SIV3D}/include"
+  sudo chown -R "$(id -u):$(id -g)" "${SIV3D}"
+  sudo cp -r "${SIV3D}/lib" "${INSTALL_PATH}"
+  sudo cp -r "${SIV3D}/include" "${INSTALL_PATH}"
+  ls "${INSTALL_PATH}/lib"
+  ls "${INSTALL_PATH}/include"
+  echo '::endgroup::'
+}
+
+install_deps
+
+install
+
+echo "path=${SIV3D}" | tee -a "${GITHUB_OUTPUT:-/dev/null}"
