f9cb681e3304f912c011301cc42d3d8c158d1d61
srz_zumix <zumix.cpp@gmail.com>
2024-05-12 20:38:01 +0900

    up

--- install-linux.sh
+++ install-linux.sh
@@ -5,13 +5,16 @@ source "${GITHUB_ACTION_PATH:-.}/resolve.sh"
 
 mkdir -p "${SIV3D_INSTALLDIR}"
 SIV3D_IMAGE="ghcr.io/srz-zumix/setup-siv3d:${INPUTS_VERSION}"
+SIV3D=${SIV3D_INSTALLDIR}/siv3d_${INPUTS_VERSION}_linux
 
 install() {
   echo '::group::📖 Download Siv3D image ...'
   docker pull "${SIV3D_IMAGE}"
   echo '::endgroup::'
   echo '::group::📖 Copy Siv3D ...'
-  docker run -it --rm -v "${SIV3D_INSTALLDIR}:/siv3d" "${SIV3D_IMAGE}" cp -r /usr/local/lib /siv3d/lib && cp -r /usr/local/include /siv3d/include
+  mkdir -p "${SIV3D}/lib"
+  mkdir -p "${SIV3D}/include"
+  docker run -it --rm -v "${SIV3D}:/siv3d" "${SIV3D_IMAGE}" cp -r /usr/local/lib /siv3d/lib && cp -r /usr/local/include /siv3d/include
   cp -r /siv3d/lib /usr/local/lib
   cp -r /siv3d/include /usr/local/include
   ls /usr/local/lib
